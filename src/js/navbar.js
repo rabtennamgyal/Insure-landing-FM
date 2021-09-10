ae1a90d82ccb27604df2dafa76b296df982d4e67
Rabten <rabten@protonmail.com>
2021-09-10 02:10:57 -0400

    created navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,26 @@
+import logo from '../images/logo.svg'
+
 function Navbar() {
     return (
-        <nav className='navbar'>
-            <h1>
-                Hi
-            </h1>
+        <nav className='navbar grid grid-two-col'>
+            <div className='navOne grid grid-jc-s grid-ai-c'>
+                <img src={logo} alt='logo' />
+            </div>
+
+            <div className='navTwo flex flex-jc-c flex-ai-c'>
+                <li>
+                    HOW WE WORK
+                </li>
+                <li>
+                    BLOG
+                </li>
+                <li>
+                    ACCOUNT
+                </li>
+                <li>
+                    VIEW PLANS
+                </li>
+            </div>
         </nav>
     )
 }
