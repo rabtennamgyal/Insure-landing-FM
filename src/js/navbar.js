fae0d6c91694b61174809151a1b2b3fcf7ed135e
Rabten <rabten@protonmail.com>
2021-09-10 05:08:33 -0400

    created mobile menu

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,24 @@
-import logo from '../images/logo.svg'
+import React, { useState } from 'react';
+import logo from '../images/logo.svg';
+import hamburger from '../images/icon-hamburger.svg';
+import close from '../images/icon-close.svg';
+
+
 
 function Navbar() {
+    const [Open, setOpen] = useState(true)
+    const [Close, setClose] = useState(false)
+
+    function openMenu() {
+        if (Close === false) {
+            setClose(true)
+            setOpen(false)
+        } else {
+            setClose(false)
+            setOpen(true)
+        }
+    }
+
     return (
         <nav className='navbar grid grid-two-col'>
             <div className='navOne grid grid-jc-s grid-ai-c'>
@@ -21,6 +39,28 @@ function Navbar() {
                     VIEW PLANS
                 </li>
             </div>
+
+            <div className='navThree flex flex-jc-fe flex-ai-c'>
+                {Open && <img src={hamburger} alt='hamburger' onClick={openMenu}/>}
+                {Close && <img src={close} alt='close' onClick={openMenu} />}
+            </div>
+
+            {Close && 
+                <div className='mobileMenu flex flex-col flex-ai-c'>
+                    <li>
+                        HOW WE WORK
+                    </li>
+                    <li>
+                        BLOG
+                    </li>
+                    <li>
+                        ACCOUNT
+                    </li>
+                    <li>
+                        VIEW PLANS
+                    </li>
+                </div>
+            }
         </nav>
     )
 }
