fd8fc7e721dd1eec21e9eca1e7604e880a02facf
Rabten <rabten@protonmail.com>
2021-09-15 19:20:45 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './js/navbar';
 import Home from './js/home';
 import Home2 from './js/home2';
+import Home3 from './js/home3';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <Home2 />
+      <Home3 />
     </div>
   );
 }
---
+++ src/js/home3.js
@@ -0,0 +1,26 @@
+function Home3() {
+    return (
+        <div className='home3 grid grid-jc-c grid-ai-c'>
+            <div className='contentBox grid grid-col-2-1 grid-pi-c'>
+                <div className='contentBoxOne grid grid-pi-c'>
+                    <h1 className='grid grid-col-one'>
+                        <span>
+                            Find out more 
+                        </span>
+                        <span>
+                            about how we <span id='work'>work</span>
+                        </span>
+                    </h1>
+                </div>
+
+                <div className='contentBoxTwo grid grid-jc-c grid-ai-c'>
+                    <button>
+                        HOW WE WORK
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home3
